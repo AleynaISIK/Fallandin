e5fcb9eb0a088d8c8d94eb157dd46aa5e4a665cf
Aleyna-ISIK <aleynaisik100@gmail.com>
2022-02-18 05:30:51 +0300

    Initial Commit

---
+++ Fallandin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fallandin
+//
+//  Created by Aleyna IŞIK on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
